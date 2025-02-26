975a0c992ad935fdf6b7442ced4cee8e104f9407
Rohan Amarasooriya <RohanA@embla.asia>
2025-02-26 09:03:51 +0530

    Add project files.

---
+++ net-api/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace net_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if (login.Username == "user" && login.Password == "password")
+            {
+                return Ok(new { message = "Login successful" });
+            }
+
+            return Unauthorized(new { message = "Invalid credentials" });
+        }
+    }
+}
---
+++ net-api/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace net_api
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
